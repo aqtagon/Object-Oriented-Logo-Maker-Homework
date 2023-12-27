b2a4ebc826f780450fc7a74c70b1a019e2ecc972
aqtagon <ahmed.danawi@gmail.com>
2023-12-27 14:22:47 -0800

    Added askQuestions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
+
+const askQuestions = async () => {
+    const answers = await inquirer.prompt([
+
+    ]);
+
+    
+}
